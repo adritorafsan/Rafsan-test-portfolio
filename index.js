831cd923c9af04197926b979f5744556c9eeab12
adrito rafsan <adritorafsan@gmail.com>
2023-07-28 12:28:42 +0600

    done

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 let sections = document.querySelectorAll('section');
-let navlinks = document.querySelectorAll('header nav a');
+let navbar = document.querySelectorAll('header nav a');
 
-window.onscroll = () =>{
+window.onscroll = ()=>{
     sections.forEach(sec =>{
         let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
+        let offset = sec.offsetTop;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
         if(top >= offset && top < offset + height){
-            navlinks.forEach(
-                links =>{
-                    links.classList.remove('active');
-                    document.querySelector('header nav a[href* = ' + id + ']').classList.add('active');
-                }
-            )
+            navbar.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ' ] ').classList.add('active');
+            })
         }
-
     })
 }
+
+
+
+const typed = new Typed('.multi-text',{
+    strings: ['Coder','Frontend Developer','Youtuber','Blogger'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay:1000,
+    loop:true
+
+})
+
